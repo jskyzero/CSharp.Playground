626192400391013c2097bbdcad545b5cf2861310
jskyzero <jskyzero@outlook.com>
2017-03-07 23:08:11 +0800

    add tode

---
+++ Todos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime NowTime = DateTime.Now;
+            Console.WriteLine(NowTime.ToString("yy/MM/dd HH:MM"));
+            Console.WriteLine("Hello World!");
+            TodoUI UI = new TodoUI();
+            UI.MainFunc();
+        }
+    }
+}
